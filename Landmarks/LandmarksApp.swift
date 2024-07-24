51ecf6549602ae5b679d3ba943cb0313564465d3
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-07-24 17:04:37 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Irvina Kočan on 24. 7. 2024..
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
